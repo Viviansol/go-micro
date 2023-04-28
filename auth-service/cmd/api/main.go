d5246cdf043ccb6813886048231cf65ccfd75365
Viviansol <viviansan93@gmail.com.br>
2023-04-28 12:04:49 -0300

    init auth service

---
+++ auth-service/cmd/api/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"auth-service/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+	log.Println("starting auth service")
+
+	app := Config{}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+
+}
---
+++ auth-service/go.mod
@@ -0,0 +1,10 @@
+module auth-service
+
+go 1.19
+
+require golang.org/x/crypto v0.8.0
+
+require (
+	github.com/go-chi/chi/v5 v5.0.8 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+)
